f3616fdc6a604c96c4df3d4dc582063409cf1ba0
Spencer Nauman <snauman817@gmail.com>
2019-02-06 22:41:35 -0500

    complete function idea created

--- manager.py
+++ manager.py
@@ -3,7 +3,7 @@ import item
 class Manager(object):
 
     def __init__(self):
-        self.list = []
+        pass
     
     def to_do(self):
         file_obj = open('todos.txt', 'r')
@@ -11,23 +11,40 @@ class Manager(object):
 
         for index in range(1, len(rows)):
             cols = rows[index].split('|')
-
-            print('-'*10)
-            print(f"Task: {cols[0]}\nCompleted: {cols[1]}\nDue: {cols[2]}\nEntered: {cols[3]}")
+            if cols[1] == 'False':
+                print('-'*10)
+                print(f"Task: {cols[0]}\nCompleted: {cols[1]}\nDue: {cols[2]}\nEntered: {cols[3]}")
 
         file_obj.close()
 
     def add(self):
-        file_obj = open('todos.txt', 'a')
-
         task = input('task: ')
         due = input("due by: ")
-
-        new_task = item.Item(task, due)
-
+        self.write_to_database(item.Item(task, due))
+    
+    def write_to_database(self, new_task):
+        file_obj = open('todos.txt', 'a')
         file_obj.write(f"\n{new_task.task}|{new_task.is_completed}|{new_task.datetime_due}|{new_task.datetime_created}")
-
         file_obj.close()
 
+    def complete(self, task):
+        task_dict = {}
+
+        file_obj = open('todos.txt', 'r+')
+        rows = file_obj.read().split('\n')
+        
+        for index in range(1, len(rows)):
+            cols = rows[index].split('|')
+            task_dict[cols[0]] = item.Item(cols[0], cols[2], cols[1], cols[3])
+        
+        task_dict[task].is_completed = True
+
+        file_obj.write('task|completed|date due|date entered')
+        file_obj.close()
+
+        for entry in task_dict:
+            self.write_to_database(entry)
+
+
 manager = Manager()
-manager.to_do()
+manager.complete('shopping')
